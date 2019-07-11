326006be5a4f4069ceac77d986bb54d801f98d24
Jodie <jodie.xiang21@hotmail.com>
2019-07-11 17:08:09 -0400

    updated tcp project

--- tcp.py
+++ tcp.py
@@ -5,20 +5,21 @@ import json
 
 
 class testPriority(object):
-    alpha = 0.7 
-    beta = 0.3
-    theta = 0.0
+    a = 0.7 
+    b = 0.3
+    c = 0.0
     name = None
-    prevPriority = 100000
-    priority = 100000
+    prevPriority = 10000
+    priority = 10000
     totalExecutions = 0
     totalFailures = 0
     totalPasses = 0
 
     def __init__(self, priorityDict=None):
-        self.name = priorityDict['name']
         if priorityDict:
+            self.name = priorityDict['name']
             self.prevPriority = priorityDict['prevPriority']
+            self.priority = priorityDict['priority']
             self.totalExecutions = priorityDict['totalExecutions']
             self.totalFailures = priorityDict['totalFailures']
             self.totalPasses = priorityDict['totalPasses']
@@ -27,14 +28,9 @@ class testPriority(object):
     #    self.name = name
 
 class testCase(object):
-        # name: "Create assignment",
-        # time: 1.232,
-        # timestamp: 2019-06-26t22:05:09,
-        # tests: 45,
-        # skipped: 0
-        # errors: 0,
-        # failures: 1
-        def __init__(self,name,time,timestamp,tests,skipped,errors,failures):
+
+        def __init__(self,filename, name,time,timestamp,tests,skipped,errors,failures):
+            self.filename=filename
             self.name=name
             self.time=time
             self.timestamp=timestamp
@@ -57,58 +53,15 @@ class testResult(object):
     resultdirectory = None
     timestamp = None
     hostname = None
-    testPriorityFile = None
     #dictionary of testCase object, with testCase.name 
     testResultHash = {}
+   # testPriorityFile = None
   
-    def _init_(self, resultdirectory, testPriorityFile=None):
+    def __init__(self, resultdirectory, timestamp, hostname, testResultHash):
         self.resultdirectory = resultdirectory
-        # check if resultdirectory is valid
-        if (os.path.isdir(resultdirectory)):
-            print(os.path.isdir(resultdirectory))
-            print("I'm here!")
-            # print(isdir(resultdirectory))
-            if not os.listdir(resultdirectory):
-                print ("empty")
-            else:
-                print ("not empty")
-        else:
-            print(os.path.isdir(resultdirectory))
-
-            #read each file in the directory and get line <testsuite...>
-            listFiles = glob.glob(resultdirectory + "/*.xml")
-            testResultHash = dict()
-            for x in listFiles:
-                try:
-                    tree = ET.parse(x)
-                    root = tree.getroot()    
-                    #parse line into variables 
-                    name = "_".join((root.attrib.get("name")).split())
-                    time = root.attrib.get("time")
-                    hostname = root.attrib.get("hostname")
-                    timestamp = root.attrib.get("timestamp")
-                    tests = root.attrib.get("tests")
-                    skipped = root.attrib.get("skipped")
-                    errors = root.attrib.get("errors")
-                    failures = root.attrib.get("failures")
-                    self.timestamp = timestamp
-                    self.hostname = hostname
-                    tc = testCase(name,time,timestamp,tests,skipped,errors,failures)
-                    #store tc in a dictionary
-                    testResultHash[tc.name] = tc
-                except Exception as e:
-                    print ("ops! can not process a file {}".format(x))
-                    print (e)
-                    print(len(x))
-                    # f = open(x, "r")
-                    # print(f.read())
-                    # f.close()
-
-            
-            for n, o in testResultHash.items():
-                print ("test {} => {}".format(n, o))
-            print ("total test cases {}".format(len(testResultHash)))
-                #check if testPriorityFile exist and has permission to write
+        self.timestamp = timestamp
+        self.hostname = hostname
+        self.testResultHash = testResultHash
 
     #populated testResult object. 
     #test_case contains all test cases
@@ -116,44 +69,45 @@ class testResult(object):
         #add code here
         pass
     
-    def write_data_to_priority(self, testPriorityFile=None):
-        if testPriorityFile is not None:
-            outfile = testPriorityFile
-        else:
-            outfile = self.testPriorityFile
+   # def write_data_to_priority(self, testPriorityFile=None):
+   #     if testPriorityFile is not None:
+   #         outfile = testPriorityFile
+   #     else:
+   #         outfile = self.testPriorityFile
 
 
 def calculatePriority(testCase, testPriority):
-    alpha = 0.7
-    beta = 0.3
     #check if testCase and testPriority are valid !!!
-    testPriority['totalExecutions'] = testPriority['totalExecutions'] + testCase.tests
-    testPriority['totalFailures'] = testPriority['totalFailures'] + testCase.failures
-    if testCase.failures > 0:
-        recentFail = 5000
+    if testCase and testPriority:
+        testPriority.totalExecutions = testPriority.totalExecutions + testCase.tests
+        testPriority.totalFailures = testPriority.totalFailures + testCase.failures
+
+        if testCase.failures > 0:
+            recentFail = 5000
+        else:
+            recentFail = 0
+        #need to take error into account !!!!
+        tcPasses = testCase.tests - testCase.failures
+        testPriority.totalPasses = testPriority.totalPasses + tcPasses
+        prevPrioritytemp = testPriority.prevPriority
+        testPriority.prevPriority = testPriority.priority
+        # if test fails on first run
+        if testPriority.totalPasses == 0:
+            recentFail = 5000
+            failRatio = 0
+        else:
+            failRatio = testPriority.totalFailures / testPriority.totalPasses
+
+        # priority_value = (testPriority.a * failRatio) + (testPriority.b * prevPrioritytemp) + recentFail
+        return ((testPriority.a * failRatio) + (testPriority.b * prevPrioritytemp) + recentFail)
     else:
-        recentFail = 0
-    #need to take error into account !!!!
-    tcPasses = testCase.tests - testCase.failures
-    testPriority['totalPasses'] = testPriority['totalPasses'] + tcPasses
-    prevPrioritytemp = testPriority['prevPriority']
-    testPriority['prevPriority'] = testCase.priority
-    # if test fails on first run
-    if testPriority['totalPasses'] == 0:
-        recentFail = 5000
-        failRatio = 0
-    else:
-        failRatio = testPriority['totalFailures'] / testPriority['totalPasses']
-        testPriority['priority'] = alpha * failRatio + beta * prevPrioritytemp + recentFail
-    # send to testPriorityFile
-    return testPriority
+        print("not valid")
+        return 10000
 
 def temp(resultdirectory):
     # check if resultdirectory is valid
     if (os.path.isdir(resultdirectory)):
-        print(os.path.isdir(resultdirectory))
         print("I'm here!")
-        # print(isdir(resultdirectory))
         if not os.listdir(resultdirectory):
              print ("empty")
         else:
@@ -161,24 +115,62 @@ def temp(resultdirectory):
     else:
         print(os.path.isdir(resultdirectory))
 
-    #read each file in the directory and get line <testsuite...>
-    listFiles = glob.glob(resultdirectory + "/*.xml")
+    #read each XML file in the directory and get line <testsuite...>
+    listXMLFiles = glob.glob(resultdirectory + "/*.xml")
+    #listJSONFiles = glob.glob(resultdirectory + "/JSON/*.json")
     testResultHash = dict()
-    for x in listFiles:
+    for x in listXMLFiles:
         try:
+            base = os.path.basename(x)
+            filename = os.path.splitext(base)[0]
             tree = ET.parse(x)
             root = tree.getroot()    
             #parse line into variables 
-            name = "_".join((root.attrib.get("name")).split())
-            time = root.attrib.get("time")
+            name = root.attrib.get("name")
+            #name = "_".join((root.attrib.get("name")).split())
+            time = float(root.attrib.get("time"))
             timestamp = root.attrib.get("timestamp")
-            tests = root.attrib.get("tests")
-            skipped = root.attrib.get("skipped")
-            errors = root.attrib.get("errors")
-            failures = root.attrib.get("failures")
-            tc = testCase(name,time,timestamp,tests,skipped,errors,failures)
-            #store tc in a dictionary
+            hostname = root.attrib.get("hostname")
+            tests = int(root.attrib.get("tests"))
+            skipped = int(root.attrib.get("skipped"))
+            errors = int(root.attrib.get("errors"))
+            failures = int(root.attrib.get("failures"))
+            #store results in a testCase
+            tc = testCase(filename,name,time,timestamp,tests,skipped,errors,failures)
+            # add testCase in a dictionary testResultHash
             testResultHash[tc.name] = tc
+            # get JSON data
+            priority_file_directory = '/Users/jxiang/Documents/TCP/JSON/'
+            priority_file_name = priority_file_directory + tc.filename + '.json'
+            if os.path.exists(priority_file_name):
+                jsonFile = open(priority_file_name, "r") # Open the JSON file for reading
+                priority_dict = json.load(jsonFile) # Read the JSON into the buffer
+                jsonFile.close()
+
+                # store in a testPriority
+                tpriority = testPriority(priority_dict)
+                tpriority.priority = calculatePriority(tc, tpriority)
+                print(tpriority.priority)
+            else: # if new test case has no JSON file
+                tpriority = testPriority()
+                tpriority.name = tc.name
+
+            priorityJSON = {
+                "a": tpriority.a,
+                "b": tpriority.b,
+                "c": tpriority.c,
+                "name": tpriority.name,
+                "prevPriority": tpriority.prevPriority,
+                "priority": tpriority.priority,
+                "totalExecutions": tpriority.totalExecutions,
+                "totalFailures": tpriority.totalFailures,
+                "totalPasses": tpriority.totalPasses
+            }
+
+            # update <testcase>.json file
+            jsonFile = open(priority_file_name, "w+")
+            jsonFile.write(json.dumps(priorityJSON))
+            jsonFile.close()
         except Exception as e:
             print ("ops! can not process a file {}".format(x))
             print (e)
@@ -186,49 +178,20 @@ def temp(resultdirectory):
             # f = open(x, "r")
             # print(f.read())
             # f.close()
+    tresult = testResult(resultdirectory, tc.timestamp, hostname, testResultHash)
+    # export testResult in some way
 
     #for n, o in testResultHash.items():
-  #      print ("test {} => {}".format(n, o))
-    print ("total test cases {}".format(len(testResultHash)))
+        #print ("test {} => {}".format(n, o))
+    #print ("total test cases {}".format(len(testResultHash)))
         
         
 
 
 if __name__ == "__main__":
-    print("This is main...")
     #temp("C:/Users/jxiang/Downloads/test-result-28june-build/test-results/test")
-    # temp("./test1")
-    # temp("\\?\C:\Users\jxiang\Downloads\build\build\test-results\test2")
-    temp("/Users/jxiang/Documents/TCP/temp")
-    priority_file_name = '/Users/jxiang/Documents/TCP/testpriority.json'
-    if os.path.exists(priority_file_name):
-        jsonFile = open(priority_file_name, "r") # Open the JSON file for reading
-        priority_dict = json.load(jsonFile) # Read the JSON into the buffer
-        jsonFile.close()
-#        with open(priority_file_name, 'r') as f:
-#            priority_dict = json.load(f)
-        test1 = testPriority(priority_dict)
-        test1.priority = calculatePriority(...)
-    else:
-        test1 = testPriority()
-        test1.name = "Scenario..."
-
-    priorityJSON = {
-        "a": 0.7,
-        "b": 0.3,
-        "c": 0.0,
-        "name": test1.name,
-        "prevPriority": test1.prevPriority,
-        "priority": test1.priority,
-        "totalExecutions": test1.totalExecutions,
-        "totalFailures": test1.totalFailures,
-        "totalPasses": test1.totalPasses
-    }
-
-    jsonFile = open(priority_file_name, "w+")
-    jsonFile.write(json.dumps(priorityJSON))
-    jsonFile.close()
-
+    #temp("/Users/jxiang/Documents/TCP/temp")
+    temp("/Users/jxiang/Documents/TCP/temp2")
     #print(calculatePriority(tc, test1.Tests[0]))
     # for x in testResultHash.values():
     #    calculatePriority(x, test1.Tests[i])
