d03d7c02b8091c062fddbac53cbfa72e9d41c5d5
Adam Wong <adam@adamwong.me>
2019-08-09 17:31:12 -0400

    Update frontend to make request

--- frontend/src/components/chart/chart-component.jsx
+++ frontend/src/components/chart/chart-component.jsx
@@ -1,39 +1,31 @@
 import React from 'react';
+import Axios from 'axios';
+
 import './chart-component.css';
 
 import {
   ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip
 } from 'recharts';
 
-const data = [
-  { name: 1, impression: 100 },
-  { name: 2, impression: 120 },
-  { name: 3, impression: 150 },
-  { name: 4, impression: 180 },
-  { name: 5, impression: 200 },
-  { name: 6, impression: 499 },
-  { name: 7, impression: 50 },
-  { name: 8, impression: 100 },
-  { name: 9, impression: 200 },
-  { name: 10, impression: 222 },
-  { name: 11, impression: 210 },
-  { name: 12, impression: 300 },
-  { name: 13, impression: 50 },
-  { name: 14, impression: 190 },
-  { name: 15, impression: 300 },
-  { name: 16, impression: 400 },
-  { name: 17, impression: 200 },
-  { name: 18, impression: 50 },
-  { name: 19, impression: 100 },
-  { name: 20, impression: 100 }
-];
 
 export default class ChartComponent extends React.PureComponent {
+  constructor(props){
+    super(props);
+    this.state = { data: null };
+  }
+  componentWillMount() {
+    Axios.get('http://localhost:5000/tests')
+    .then((response) => {
+      console.log(response.data);
+      this.setState({data : response.data});
+    });
+  }
+
   render() {
     return (
       <div className="chart">
         <ResponsiveContainer style>
-          <LineChart data={data}>
+          <LineChart data={this.state.data}>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="name" />
             <YAxis dataKey="impression" />
