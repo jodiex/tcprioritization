2dada92e8a55c6270aef0d92c7ce5a24ce599b87
Adam Wong <adam@adamwong.me>
2019-08-09 17:28:02 -0400

    Chart pulls from API

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from routes.tests import tests
+from flask_cors import CORS
 
 app = Flask("QABackend")
+CORS(app)
+
 app.register_blueprint(tests, url_prefix='/tests')
 
 @app.route('/')
 def HelloWordl():
     return "Hello World!"
-
-
-
-print(__name__)
-    #app.run()
